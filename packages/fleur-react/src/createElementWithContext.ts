ed325bfb0dae200faca190dd32e82a61bac68912
ragg <ra-gg@users.noreply.github.com>
2018-07-31 03:44:32 +0900

    Fix invalid type generation

--- packages/fleur-react/src/createElementWithContext.ts
+++ packages/fleur-react/src/createElementWithContext.ts
@@ -1,9 +1,9 @@
-import { AppContext } from '@ragg/fleur'
+import { AppContext, ComponentContext } from '@ragg/fleur'
 import * as React from 'react'
 
 import ComponentContextProvider from './ComponentContextProvider'
 
-const createElementWithContext = <P>(context: AppContext<any>, Component: React.ComponentClass<P>, props?: P) => (
+const createElementWithContext = <P>(context: AppContext<any>, Component: React.ComponentClass<P>, props?: P): React.ReactElement<React.ProviderProps<ComponentContext>> => (
     React.createElement(ComponentContextProvider.Provider, {
         value: context.componentContext
     }, React.createElement(Component, (props || {} as any)))
