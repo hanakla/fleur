5cabc34c10e25b7b2d30dcb9f694157d043cb675
ragg <ra-gg@users.noreply.github.com>
2018-07-31 03:05:27 +0900

    Fix withComponentContext test (update enzyme-adapter-react-16)

--- packages/fleur-react/src/withComponentContext.spec.ts
+++ packages/fleur-react/src/withComponentContext.spec.ts
@@ -1,7 +1,6 @@
 import Fleur from '@ragg/fleur'
 import { mount } from 'enzyme'
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
 
 import { createElementWithContext } from './createElementWithContext'
 import withComponentContext, { ContextProp } from './withComponentContext'
@@ -10,17 +9,21 @@ describe('withComponentContext', () => {
     it('Is received context in props', async () => {
         const app = new Fleur()
         const context = app.createContext()
-        const Component = withComponentContext(class extends React.Component<ContextProp> { public render() {
-            expect(Object.keys(this.props.context)).toEqual(['executeOperation', 'getStore'])
-            return null
-        } })
-
-        const div = document.createElement('div')
-        await new Promise (resolve => {
-            const component = ReactDOM.render(createElementWithContext(context, Component), div, resolve)
+        const Component = withComponentContext(class extends React.Component<ContextProp> {
+            public render() {
+                return React.createElement('div')
+            }
         })
 
+        const wrapper = mount(createElementWithContext(context, Component, { prop: 'prop string' }))
+
         // TODO: Wait for React 16 suppors in Enzyme
-        // expect(mount(createElementWithContext(context, Component, {})).props).toMatchObject({ context: context.componentContext })
+        expect(wrapper.find('Component').props()).toEqual({
+            context: {
+                executeOperation: context.componentContext.executeOperation,
+                getStore: context.componentContext.getStore,
+            },
+            prop: 'prop string'
+        })
     })
 })
