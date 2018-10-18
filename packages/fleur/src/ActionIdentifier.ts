39f3648e347427993484d4979a4053b9f8cfaee3
ragg <ragg.devpr@gmail.com>
2018-10-18 15:54:40 +0900

    Branded typing for ActionIdentifier

--- packages/fleur/src/ActionIdentifier.ts
+++ packages/fleur/src/ActionIdentifier.ts
@@ -1,9 +1,9 @@
-export type ActionIdentifier<P> = () => P
+export type ActionIdentifier<P> = (() => P) & { __actionIdentifier: never }
 export type ExtractPayloadType<T extends ActionIdentifier<any>> = ReturnType<T>
 export type ExtractActionIdentifiersFromObject<T extends object> = T[keyof T]
 
-const action = <P>(): ActionIdentifier<P> => (_?: P): P => {
-    throw new Error('Do not call Action as function')
-}
+const action = <P>(): ActionIdentifier<P> => ((): P => {
+    throw new Error('Do not call ActionIdentifier as function')
+}) as ActionIdentifier<P>
 
 export { action }
