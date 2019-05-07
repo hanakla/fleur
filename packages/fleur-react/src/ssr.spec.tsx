c7c12d6cd5a41dfeb7d032387ac20488e21a7788
ragg <ragg.devpr@gmail.com>
2019-05-07 20:28:19 +0900

    Restore SSR spec

--- packages/fleur-react/src/ssr.spec.tsx
+++ packages/fleur-react/src/ssr.spec.tsx
@@ -14,91 +14,89 @@ describe('Sever side rendering', () => {
   let app: Fleur
 
   beforeAll(() => {
-    // const increaseIdent = action<{ increase: number }>()
-    // const increaseOp = operation((ctx, { increase }: { increase: number }) => {
-    //   ctx.dispatch(increaseIdent, { increase })
-    // })
-    // class TestStore extends Store {
-    //   public static storeName = 'TestStore'
-    //   protected state = { count: 0 }
-    //   private increase = listen(increaseIdent, ({ increase }) => {
-    //     this.updateWith(d => (d.count += increase))
-    //   })
-    //   public getCount() {
-    //     return this.state.count
-    //   }
-    // }
-    // const Component = connectToStores([TestStore], getStore => ({
-    //   count: getStore(TestStore).getCount(),
-    // }))(
-    //   class extends React.PureComponent<{ count: number } & ContextProp> {
-    //     public render() {
-    //       return <div>Your count {this.props.count}</div>
-    //     }
-    //   },
-    // )
-    // app = new Fleur()
-    // app.registerStore(TestStore)
-    // const serverApp = express()
-    // serverApp.get('/', async (req, res) => {
-    //   try {
-    //     const context = app.createContext()
-    //     context.getStore(TestStore)
-    //     await context.executeOperation(increaseOp, {
-    //       increase: parseInt(req.query.amount, 10),
-    //     })
-    //     res.write(
-    //       ReactDOMServer.renderToString(
-    //         <html>
-    //           <head>
-    //             <script data-state={JSON.stringify(context.dehydrate())} />
-    //           </head>
-    //           <body>{createElementWithContext(context, Component, {})}</body>
-    //         </html>,
-    //       ),
-    //     )
-    //     res.end()
-    //   } catch (e) {
-    //     server && server.close()
-    //     throw e
-    //   }
-    // })
-    // server = serverApp.listen(31987)
-  })
+    const increaseIdent = action<{ increase: number }>()
 
-  afterAll(() => {
-    server && server.close()
+    const increaseOp = operation((ctx, { increase }: { increase: number }) => {
+      ctx.dispatch(increaseIdent, { increase })
+    })
+
+    class TestStore extends Store {
+      public static storeName = 'TestStore'
+      protected state = { count: 0 }
+      private increase = listen(increaseIdent, ({ increase }) => {
+        this.updateWith(d => (d.count += increase))
+      })
+      public getCount() {
+        return this.state.count
+      }
+    }
+
+    const Component = connectToStores([TestStore], getStore => ({
+      count: getStore(TestStore).getCount(),
+    }))((props: { count: number } & ContextProp) => (
+      <div>Your count {props.count}</div>
+    ))
+
+    app = new Fleur({ stores: [TestStore] })
+
+    const serverApp = express()
+    serverApp.get('/', async (req, res) => {
+      try {
+        const context = app.createContext()
+
+        await context.executeOperation(increaseOp, {
+          increase: parseInt(req.query.amount, 10),
+        })
+
+        res.write(
+          ReactDOMServer.renderToString(
+            <html>
+              <head>
+                <script data-state={JSON.stringify(context.dehydrate())} />
+              </head>
+              <body>{createElementWithContext(context, Component, {})}</body>
+            </html>,
+          ),
+        )
+
+        res.end()
+      } catch (e) {
+        server && server.close()
+        throw e
+      }
+    })
+    server = serverApp.listen(31987)
   })
 
   it('test', async () => {
-    // try {
-    //   // First request
-    //   const res1 = await request.get('http://localhost:31987/?amount=10')
-    //   const dehydratedState1 = JSON.parse(
-    //     cheerio
-    //       .load(res1)('script')
-    //       .attr('data-state'),
-    //   )
-    //   expect(res1).toContain('<div>Your count 10</div>')
-    //   expect(dehydratedState1).toEqual({ stores: { TestStore: { count: 10 } } })
-    //   const clientContext1 = app.createContext()
-    //   clientContext1.rehydrate(dehydratedState1)
-    //   expect(clientContext1.getStore('TestStore').state).toEqual({ count: 10 })
-    //   // Another request
-    //   const res2 = await request.get('http://localhost:31987/?amount=20')
-    //   const dehydratedState2 = JSON.parse(
-    //     cheerio
-    //       .load(res2)('script')
-    //       .attr('data-state'),
-    //   )
-    //   expect(res2).toContain('<div>Your count 20</div>')
-    //   expect(dehydratedState2).toEqual({ stores: { TestStore: { count: 20 } } })
-    //   const clientContext2 = app.createContext()
-    //   clientContext2.rehydrate(dehydratedState2)
-    //   expect(clientContext2.getStore('TestStore').state).toEqual({ count: 20 })
-    // } catch (e) {
-    //   server && server.close()
-    //   throw e
-    // }
+    try {
+      // First request
+      const res1 = await request.get('http://localhost:31987/?amount=10')
+      const dehydratedState1 = JSON.parse(
+        cheerio
+          .load(res1)('script')
+          .attr('data-state'),
+      )
+      expect(res1).toContain('<div>Your count 10</div>')
+      expect(dehydratedState1).toEqual({ stores: { TestStore: { count: 10 } } })
+      const clientContext1 = app.createContext()
+      clientContext1.rehydrate(dehydratedState1)
+      expect(clientContext1.getStore('TestStore').state).toEqual({ count: 10 })
+      // Another request
+      const res2 = await request.get('http://localhost:31987/?amount=20')
+      const dehydratedState2 = JSON.parse(
+        cheerio
+          .load(res2)('script')
+          .attr('data-state'),
+      )
+      expect(res2).toContain('<div>Your count 20</div>')
+      expect(dehydratedState2).toEqual({ stores: { TestStore: { count: 20 } } })
+      const clientContext2 = app.createContext()
+      clientContext2.rehydrate(dehydratedState2)
+      expect(clientContext2.getStore('TestStore').state).toEqual({ count: 20 })
+    } catch (e) {
+      server && server.close()
+      throw e
+    }
   })
 })
