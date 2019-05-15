29fdd80a928c6dca97ddc6695063bf4c80bc128b
Mitsuka Hanakura aka ragg <ragg.devpr@gmail.com>
2019-05-15 19:33:23 +0900

    WIP

--- packages/fleur/src/Store.ts
+++ packages/fleur/src/Store.ts
@@ -3,16 +3,23 @@ import immer, { Draft } from 'immer'
 import { ActionIdentifier, ExtractPayloadType } from './Action'
 import Emitter from './Emitter'
 import { StoreContext } from './StoreContext'
+import { action } from '../typings'
 
 export interface StoreClass<T = {}> {
   storeName: string
   new (context: StoreContext): Store<T>
 }
 
+type DispatchListener = {
+  __fleurHandler: true
+  __action: ActionIdentifier<any>
+  producer: (payload: object) => void
+}
+
 export const listen = <A extends ActionIdentifier<any>>(
   action: A,
   producer: (payload: ExtractPayloadType<A>) => void,
-) => ({
+): DispatchListener => ({
   __fleurHandler: true,
   __action: action,
   producer,
@@ -52,3 +59,43 @@ export default class Store<T = any> extends Emitter<StoreEvents> {
     this.context.enqueueToUpdate(this)
   }
 }
+
+// type Reducer
+
+// export const store = <T>(storeName: string, reducer: ) => {
+//   return class extends Store<T> {
+//     public static storeName = storeName
+
+//     constructor(context: StoreContext) {
+//       super(context)
+
+//       defs.forEach((listener, idx) => {
+//         ;(this as any)[`__listener$${idx}`] = listener
+//       })
+//     }
+//   }
+// }
+
+const a = action<{ a: string }>()
+
+type State = { b: string }
+
+const store = <S>(storeName: string) => {
+
+  return class extends Store<S> {
+    public static storeName = storeName
+    public static listeners = []
+    public static listen<T extends ActionIdentifier<any>(ident: T, producer: (payload: ExtractPayloadType<T>, state: State) => void) {
+
+    }
+
+    constructor(context: StoreContext) {
+      super(context)
+      this
+    }
+  }
+}
+
+store<State>('TestStore')
+  .listen(a, (state) => {})
+  .listen(a, (state) => {})
