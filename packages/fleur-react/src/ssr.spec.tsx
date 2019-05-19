9932b690a716769caaffdde027bb3371692907cb
ragg <ra-gg@users.noreply.github.com>
2019-05-19 22:54:12 +0900

    Remove unused imports

--- packages/fleur-react/src/ssr.spec.tsx
+++ packages/fleur-react/src/ssr.spec.tsx
@@ -6,8 +6,6 @@ import * as React from 'react'
 import * as ReactDOMServer from 'react-dom/server'
 import * as request from 'request-promise'
 
-import { ContextProp, createElementWithContext } from '.'
-import connectToStores from './connectToStores'
 import { useStore } from './useStore'
 import { FleurContext } from './ComponentContextProvider'
 
