c89d8741e86524760df9e669d7c3c5b2650821c2
Mitsuka Hanakura <ragg.devpr@gmail.com>
2019-05-23 22:41:44 +0900

    Example for websocket connection

--- packages/fleur/src/keepAliveOperation.spec.ts
+++ packages/fleur/src/keepAliveOperation.spec.ts
@@ -2,30 +2,41 @@ import { keepAliveOperation } from './keepAliveOperation'
 import { Fleur } from './Fleur'
 import { action } from './Action'
 import { OperationContext } from './OperationContext'
+import Emitter from './Emitter'
 
 describe('keepAliveOperation', () => {
   const app = new Fleur()
   const context = app.createContext()
-  const ident = action<string>()
+  const ident = action<any>()
 
   it('Should work', async () => {
     const dispatchSpy = jest.spyOn(context, 'dispatch')
 
     const disposer = jest.fn()
-    const spy = jest.fn((context: OperationContext, userId: number) => {
-      // Setup here, like socket.connect
-      setTimeout(() => context.dispatch(ident, 'payload'), 100)
+    const spy = jest.fn(
+      (context: OperationContext, connection: Emitter<any>, userId: number) => {
+        // Listen events
+        connection.on('message', data => {
+          context.dispatch(ident, { data, userId })
+        })
 
-      // Return disposer, likes () => socket.disconnect()
-      return disposer
-    })
+        // Return disposer, likes `() => connection.off()`, `() => clearInterval(pollingId)`
+        // or any disposing process
+        return () => {
+          connection.off('message')
+          disposer()
+        }
+      },
+    )
+
+    const connectionMock = new Emitter<any>()
 
     const op = keepAliveOperation(spy)
-    await context.executeOperation(op, 1)
-    expect(spy).toBeCalledWith(context.operationContext, 1)
+    await context.executeOperation(op, connectionMock, 1)
+    expect(spy).toBeCalledWith(context.operationContext, connectionMock, 1)
 
-    await new Promise(r => setTimeout(r, 500))
-    expect(dispatchSpy).toBeCalledWith(ident, 'payload')
+    connectionMock.emit('message', 'message')
+    expect(dispatchSpy).toBeCalledWith(ident, { data: 'message', userId: 1 })
 
     await context.executeOperation(op.dispose)
     expect(disposer).toBeCalled()
