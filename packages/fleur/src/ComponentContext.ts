30597077f7444fe708ede914a3a32a8c87de6a75
Hanakla <ragg.devpr@gmail.com>
2019-11-13 20:03:54 +0900

    Simplify ComponentContext and OperationContext

--- packages/fleur/src/ComponentContext.ts
+++ packages/fleur/src/ComponentContext.ts
@@ -1,18 +1,11 @@
-import { AppContext } from './AppContext'
 import { Operation, OperationArgs } from './Operations'
 import { StoreClass } from './Store'
 
-export class ComponentContext {
-  constructor(private context: AppContext) {}
-
-  public executeOperation = <O extends Operation>(
+export interface ComponentContext {
+  executeOperation<O extends Operation>(
     operation: O,
     ...args: OperationArgs<O>
-  ): void => {
-    this.context.executeOperation(operation, ...args)
-  }
+  ): void | undefined
 
-  public getStore = <T extends StoreClass>(StoreClass: T): InstanceType<T> => {
-    return this.context.getStore(StoreClass)
-  }
+  getStore<T extends StoreClass>(StoreClass: T): InstanceType<T>
 }
