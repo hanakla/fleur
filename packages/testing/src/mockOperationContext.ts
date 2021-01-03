90c950e3f55b046e3d1915f77ae48a633405e050
Hanakla <8597982+hanakla@users.noreply.github.com>
2021-01-03 13:40:07 +0900

    Remove MockOperationContext class (empty class)

--- packages/testing/src/mockOperationContext.ts
+++ packages/testing/src/mockOperationContext.ts
@@ -1,15 +1,5 @@
-import { Operation, OperationArgs } from '@fleur/fleur'
 import { MockStore } from './mockStore'
 import { MockContextBase } from './MockContextBase'
-
-export class MockOperationContext extends MockContextBase {
-  public executeOperation = async <O extends Operation>(
-    operation: O,
-    ...args: OperationArgs<O>
-  ): Promise<void> => {
-    await Promise.resolve(operation(this as any, ...args))
-  }
-}
 
 export const mockOperationContext = ({
   stores,
@@ -17,6 +7,6 @@ export const mockOperationContext = ({
 }: {
   stores: readonly MockStore[]
   mocks: Map<any, any>
-}): MockOperationContext => {
-  return new MockOperationContext({ stores, mocks })
+}): MockContextBase => {
+  return new MockContextBase({ stores, mocks })
 }
