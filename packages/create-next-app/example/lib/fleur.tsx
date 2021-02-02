782378a34b6b73848bf95a6a979ede08108900ef
Hanakla <8597982+hanakla@users.noreply.github.com>
2021-02-02 16:47:05 +0900

    Fix pageProps assignment
    Co-authored-by: jigsaw <jigsaw@live.jp>

--- packages/create-next-app/example/lib/fleur.tsx
+++ packages/create-next-app/example/lib/fleur.tsx
@@ -88,7 +88,10 @@ export function appWithFleur<P>(
 
       return {
         ...appProps,
-        pageProps,
+        pageProps: {
+          __FLEUR_STATE__: serializeContext(fleurContext),
+          ...pageProps,
+        },
       }
     }
   }
